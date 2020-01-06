961735f5da523b96b3cdb9196c62103bf8ea8ced
Artem <karpuxov.a@mail.ru>
2020-01-06 08:32:17 +0300

    auth + client

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'materialize-css';
+
+const App: React.FC = () => {
+  return (
+    <div className="container">
+
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/modules/Auth.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+export const Auth: FC = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
